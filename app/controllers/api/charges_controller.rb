a0d98ef7c34e848af1e9015b44866c251ea2fcd1
adamking314 <king.adam314@gmail.com>
2025-05-18 15:23:48 -0700

    checking mark complete

--- app/controllers/api/charges_controller.rb
+++ app/controllers/api/charges_controller.rb
@@ -80,41 +80,31 @@ module Api
     
 
     def mark_complete
-      # You can find your endpoint's secret in your webhook settings
       endpoint_secret = ENV['STRIPE_MARK_COMPLETE_WEBHOOK_SIGNING_SECRET']
-
+    
       event = nil
-
-      # Verify webhook signature and extract the event
-      # See https://stripe.com/docs/webhooks/signatures for more information.
+    
       begin
         sig_header = request.env['HTTP_STRIPE_SIGNATURE']
         payload = request.body.read
-        event = Stripe::Webhook.construct_event(
-          payload, sig_header, endpoint_secret
-        )
-      rescue JSON::ParserError => e
-        # Invalid payload
-        return head :bad_request
-      rescue Stripe::SignatureVerificationError => e
-        # Invalid signature
+        event = Stripe::Webhook.construct_event(payload, sig_header, endpoint_secret)
+      rescue JSON::ParserError, Stripe::SignatureVerificationError
         return head :bad_request
       end
-
-      # Handle the checkout.session.completed event
+    
       if event['type'] == 'checkout.session.completed'
         session = event['data']['object']
-
-        # Fulfill the purchase, mark related charge as complete
         charge = Charge.find_by(checkout_session_id: session.id)
-        return head :bad_request if !charge
-
-        charge.update({ complete: true })
-
+        return head :bad_request unless charge
+    
+        charge.update(complete: true)
+    
+        # ✅ Mark booking as paid
+        booking = charge.booking
+        booking.update(paid: true) if booking
+    
         return head :ok
       end
-
-      return head :bad_request
+    
+      head :bad_request
     end
-  end
-end
