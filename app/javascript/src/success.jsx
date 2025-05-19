fbc90353158aeb3b36338a95a51bf9bfdfe68422
adamking314 <king.adam314@gmail.com>
2025-05-18 17:51:11 -0700

    testing to see if it passes

--- app/javascript/src/success.jsx
+++ app/javascript/src/success.jsx
@@ -9,7 +9,11 @@ class SuccessPage extends React.Component {
     const bookingId = this.props.bookingId;
     fetch(`/api/bookings/${bookingId}`)
       .then(r => r.json())
-      .then(data => this.setState({ booking: data, loading: false }))
+      .then(data => {
+        console.log("Fetched booking:", data);
+        this.setState({ booking: data, loading: false })
+      }
+      )
       .catch(() => this.setState({ error: 'Failed to load booking', loading: false }));
   }
 
